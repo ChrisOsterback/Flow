c7ca7a95977ef5367f1282dd69bc1a22f84a5347
Chris <Christian.Osterback@gmail.com>
2019-06-18 18:31:53 +0300

    animation changes, not final

--- FlowEjs/config/auth.js
+++ FlowEjs/config/auth.js
@@ -3,7 +3,7 @@ exports.isAdmin = function(req, res, next) {
     if (req.isAuthenticated() && res.locals.user.admin == 1) {
         next();
     } else {
-        req.flash('danger', 'Please log in as admin.');
+        req.flash('Success', 'Welcome To Admin Page');
         res.redirect('/users/login');
     }
 }
